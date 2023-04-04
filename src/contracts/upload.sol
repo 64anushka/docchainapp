251390bd4b142931dd64478d64a0f95ba1ccec81
maheshlode <maheshlode55@gmail.com>
2023-04-04 10:50:17 +0530

    display backend

--- src/contracts/upload.sol
+++ src/contracts/upload.sol
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: GPL-3.0
+
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Upload {
@@ -14,4 +16,10 @@ contract Upload {
   function add(address _user,string memory url) external {
       value[_user].push(url);
   }
+
+  function display(address _user) external view returns(string[] memory){
+      require(_user==msg.sender || ownership[_user][msg.sender],"You don't have access");
+      return value[_user];
+  }
 }
+
