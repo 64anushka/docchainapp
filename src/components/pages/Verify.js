d99bc42b15a671ff4a60c94eb3b371c80e1e5ba6
Chetana200 <agarwalchetana08@gmail.com>
2023-04-11 10:10:24 +0530

    verify-middleware

--- src/components/pages/Verify.js
+++ src/components/pages/Verify.js
@@ -1,5 +1,62 @@
 import React from "react";
 import "../css/Verify.css";
+const Verify = ({ contract, account, provider }) => {
+    const [file, setFile] = useState(null);
+    const [fileName, setFileName] = useState("No image selected");
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      if (file) {
+        try {
+          const formData = new FormData();
+          formData.append("file", file);
+          const resFile = await axios({
+            method: "post",
+            url: "https://api.pinata.cloud/pinning/pinFileToIPFS",
+            data: formData,
+            headers: {
+                pinata_api_key: `b2e3a82cf3f882bc0aeb`,
+                pinata_secret_api_key: `7a2aff56b95a1db24887b2e9ae2ccc2379ea662ac0fbc324a851839a4edb86f2`,
+                "Content-Type": "multipart/form-data"
+            }
+        });
+          const ImgHash = `${resFile.data.IpfsHash}`;
+          //const signer = contract.connect(provider.getSigner());
+          const signer = contract.connect(provider.getSigner());
+          const result = await signer.verifyImage(ImgHash); 
+          console.log(result);
+          console.log(typeof(result));
+          const res = JSON.parse(JSON.stringify(result))
+          console.log(res);
+          //alert(result);
+          if(res)
+          {
+            document.getElementById("result").innerHTML = "Verified Successfully!";
+          }
+          else
+          {
+            document.getElementById("result").innerHTML = "Unsuccessful!";
+          }
+        } catch (e) {
+          console.log(e);
+         // alert("Unable to Verify image");
+          document.getElementsById("result").innerHTML = "Unsuccessfully!";
+        }
+      }
+      
+      setFileName("No image selected");
+      setFile(null);
+    };
+    const retrieveFile = (e) => {
+      const data = e.target.files[0]; //files array of files object
+      // console.log(data);
+      const reader = new window.FileReader();
+      reader.readAsArrayBuffer(data);
+      reader.onloadend = () => {
+        setFile(e.target.files[0]);
+      };
+      setFileName(e.target.files[0].name);
+      e.preventDefault();
+    };
 
 return (
     <center>
