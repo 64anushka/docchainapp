034ae1f1dad1f2e9474ccce699892a5148fbaf26
Chetana200 <agarwalchetana08@gmail.com>
2023-04-11 10:07:22 +0530

    dependencies

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import {
 } from "react-router-dom";
 import { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import HomePage from './components/pages/home';
+import HomePage from './components/pages/Home';
 
 
 function App() {
